16395b3420a26a35ba04c8d911c4ca7b19b6b0d3
rafiq2704 <rafiq2704@gmail.com>
2020-11-27 17:17:14 +0800

    Update test archive tomcat.sh

--- test archive tomcat.sh
+++ test archive tomcat.sh
@@ -2,10 +2,9 @@
 
 pid=$(ps -ef | grep tomcat | grep java | awk ' { print $2 } ')
 
-do
-    kill -9 ${pid}
-    mv catalina.out /backup
-    tar catalina.out.$(date '+%Y%m%d').tar
-    rm -rf catalina.out
-    echo "Running out of space \"$partition ($usep%)\" on $(hostname) as on $(date)" | tee >> ~/healthcheck/archive_tomcat_$(date '+%Y%m%d').log
-done
+kill -9 ${pid}
+mv ~/opt/tomcat/logs/catalina.out ~/opt/tomcat/logs/backup
+sh ~/opt/tomcat/bin/startup.sh
+tar ~/opt/tomcat/logs/backup/catalina.out.$(date '+%Y%m%d').tar ~/opt/tomcat/logs/backup/catalina.out
+rm -rf ~/opt/tomcat/logs/backup/catalina.out
+echo "Running out of space \"$partition ($usep%)\" on $(hostname) as on $(date)" | tee >> ~/opt/tomcat/logs/archive_tomcat_$(date '+%Y%m%d').log
