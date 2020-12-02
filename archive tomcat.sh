c06374c85fff32297fd052c5ebe3e37d5db10ddd
rafiq2704 <rafiq2704@gmail.com>
2020-12-02 13:01:21 +0800

    Update archive tomcat.sh
    put kill function inside main if

--- archive tomcat.sh
+++ archive tomcat.sh
@@ -2,17 +2,16 @@
 df -H | grep -vE '^Filesystem|tmpfs|cdrom' | awk '{ print $5 " " $1 }' | while read output;
 pid=$(ps -ef | grep tomcat | grep java | awk ' { print $2 } ')
 
-#Loop to kill all pid (if it has multiple instances)
-for i in "${pid}"
-do
-  kill -9 ${pid}
-done
-
 do
   echo $output
   usep=$(echo $output | awk '{ print $1}' | cut -d'%' -f1  )
   partition=$(echo $output | awk '{ print $2 }' )
   if [ $usep -ge 70 ]; then
+    #Loop to kill all pid (if it has multiple instances)
+    for i in "${pid}"
+      do
+        kill -9 ${pid}
+      done
     mv /opt/tomcat/logs/catalina.out /opt/tomcat/logs/backup
     sh /opt/tomcat/bin/startup.sh
     tar -czvf /opt/tomcat/logs/backup/catalina.out.$(date '+%Y%m%d').tar /opt/tomcat/logs/backup/catalina.out
