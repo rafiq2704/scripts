968e2820dbeefee7c7d9bd1b2784cae5a7815395
rafiq2704 <rafiq2704@gmail.com>
2020-12-03 17:16:19 +0800

    Update archive tomcat.sh
    retrieve output before do

--- archive tomcat.sh
+++ archive tomcat.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-df -H | grep -vE '^Filesystem|tmpfs|cdrom' | awk '{ print $5 " " $1 }' | while read output;
+#Retrieve all running Process ID (PID)
 pid=$(ps -ef | grep tomcat | grep java | awk ' { print $2 } ')
 
 kill_pid()
@@ -7,10 +7,11 @@ kill_pid()
   #Loop to kill all pid (if it has multiple instances)
     for i in "${pid}"
     do
-       kill -9 ${pid}
+      kill -9 ${pid}
     done
 }
 
+df -H | grep -vE '^Filesystem|tmpfs|cdrom' | awk '{ print $5 " " $1 }' | while read output;
 do
   echo $output
   usep=$(echo $output | awk '{ print $1}' | cut -d'%' -f1  )
@@ -22,7 +23,8 @@ do
     tar -czvf /opt/tomcat/logs/backup/catalina.out.$(date '+%Y%m%d').tar /opt/tomcat/logs/backup/catalina.out
     rm -rf /opt/tomcat/logs/backup/catalina.out
     echo "Running out of space \"$partition ($usep%)\" on $(hostname) as on $(date)" | tee >> ~/healthcheck/archive_tomcat_$(date '+%Y%m%d').log
+     echo "Test"
   else
-	  echo "Usage space is less than 70%"
+     echo "Usage space is less than 80%"
   fi
 done
