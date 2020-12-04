347db160a5d873cc13d19575bc8dd50b5730b716
rafiq2704 <rafiq2704@gmail.com>
2020-12-04 11:34:40 +0800

    Create old logs housekeeping.sh
    to clear files more than 60 days in /opt/tomcat/logs/backup

---
+++ old logs housekeeping.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# A table that contains the path of directories to clean
+rep_log=("/opt/tomcat/logs/backup")
+echo "Cleaning logs - $(date)." tee >> ~/healthcheck/old_logs_houskeeping_$(date '+%Y%m%d').log
+
+#loop for each path provided by rep_log 
+for element in "${rep_log[@]}"
+do
+   #display the directory
+    echo "$element";
+    nb_log=$(find "$element" -type f -mtime +60 -name "*.log*"| wc -l)
+    if [[ $nb_log != 0 ]] 
+    then
+            find "$element" -type f -mtime +60 -delete 
+            echo "Successfully cleaned old logs." tee >> ~/healthcheck/old_logs_houskeeping_$(date '+%Y%m%d').log
+    else
+            echo "No logs to clean!" tee >> ~/healthcheck/old_logs_houskeeping_$(date '+%Y%m%d').log
+    fi
+done
